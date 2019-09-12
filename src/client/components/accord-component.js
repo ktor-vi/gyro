6b9bd56722a8bb1d54276fa9a1b7ab6badf182c7
Kardiah <winckler.p@gmail.com>
2019-09-12 15:03:08 +0200

    Test des components

--- src/client/components/accord-component.js
+++ src/client/components/accord-component.js
@@ -1,5 +1,11 @@
 import * as React from "react";
 
-const Accord = () => <div />;
+const Accord = () => (
+    <div>
+        <h1>{"Accord"}</h1>
+        <hr />
+        <small>{"becode/trouvkach"}</small>
+    </div>
+);
 
 export default Accord;
