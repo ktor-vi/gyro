a2a16d1ef990159851e9344f520f9a2e8be4e227
Kardiah <winckler.p@gmail.com>
2019-09-17 14:31:31 +0200

    First Test

--- src/client/components/hello.js
+++ src/client/components/hello.js
@@ -10,7 +10,7 @@ import * as React from "react";
 
 const HelloWorld = () => (
     <div>
-        <h1>{"Hello, world!"}</h1>
+        <h1>{"Hello!"}</h1>
         <hr />
         <small>{"becode/trouvkach"}</small>
     </div>
